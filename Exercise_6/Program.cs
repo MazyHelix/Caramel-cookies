560c3533de8a0e002f85a0baffd7ee401f616da0
LidiaVolkova <lidok86@gmail.com>
2022-06-28 18:10:03 +0300

    Задача 6

---
+++ Exercise_6/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число");
+int x = Convert.ToInt32(Console.ReadLine());
+if ((x % 2) == 0)
+{
+    Console.WriteLine("да");
+ }
+else
+{
+    Console.WriteLine("нет");
+}
