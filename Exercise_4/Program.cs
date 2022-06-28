9e4ce831512da2215faffd38e4265ba955717248
LidiaVolkova <lidok86@gmail.com>
2022-06-28 18:58:41 +0300

    Задача 4

---
+++ Exercise_4/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите число a");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число b");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число c");
+int c = Convert.ToInt32(Console.ReadLine());
+int max = a;
+if (b > max)
+{
+   max = b;
+}
+if(c > max)
+{
+   max = c; 
+}
+else
+{
+    max = a;
+}
+Console.WriteLine($"max = {max}");
