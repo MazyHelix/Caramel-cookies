6bb9b952e5fbbc19cc67f6724f3ecd312dbe8d88
LidiaVolkova <lidok86@gmail.com>
2022-06-28 16:13:44 +0300

    Задача 2

---
+++ Exercise_2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число a");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число b");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a > b)
+{
+    Console.WriteLine(a);
+}
+else
+{
+    Console.Write("max = ");
+    Console.WriteLine(b);
+}
