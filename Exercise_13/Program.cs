cfcb1f782d7f957f1248d066de86e9fe6c20c56b
LidiaVolkova <lidok86@gmail.com>
2022-07-02 21:50:01 +0300

    Задача 13

---
+++ Exercise_13/Program.cs
@@ -0,0 +1,15 @@
+﻿void findingNumber(int numberA)
+{
+    if(numberA > 99)
+    {
+        Console.WriteLine("Третьей цифрой числа " + numberA + " является " + numberA % 10);
+    }
+    else
+    {
+        Console.WriteLine("Третьей цифры числа " + numberA + " нет");
+    }
+}
+
+findingNumber(645);
+findingNumber(78);
+//findingNumber(32679); А вот с этим числом проблема(
