ca34987083f33c50f9234efaa96bb062324ea9ca
LidiaVolkova <lidok86@gmail.com>
2022-07-02 21:52:17 +0300

    Задача 15

---
+++ Exercise_15/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите цифру, обозначающую день недели");
+int dayNum = Convert.ToInt32(Console.ReadLine());
+
+void dayOfTheWeek(int dayNum) 
+{
+    if (dayNum == 6 || dayNum == 7) 
+    {
+        Console.WriteLine("Это выходной!");
+    }
+    else if (dayNum < 1 || dayNum > 7) 
+    {
+        Console.WriteLine("Ошибка! Введите другую цифру");
+    }
+  else Console.WriteLine("Это рабочий день!");
+}
+
+dayOfTheWeek(dayNum);
