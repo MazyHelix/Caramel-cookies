73aef4ee47c707190d523df6d57d86fe23a5e9d3
LidiaVolkova <lidok86@gmail.com>
2022-07-02 21:47:06 +0300

    Задача 10

---
+++ Exercise_10/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача № 10
+
+void randomSecondNumber(int start, int finish)
+{
+    int numberA = new Random().Next(start,finish);
+    Console.WriteLine(numberA);
+    int secondnum = (numberA /10) % 10;
+    Console.WriteLine("Вторая цифра числа " + numberA + " является " + secondnum);
+}
+
+randomSecondNumber(100,999);
