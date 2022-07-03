fbe3fc8d3a4a7a9b1b99b623f409b1b4015f10c2
LidiaVolkova <lidok86@gmail.com>
2022-07-04 00:51:05 +0300

    Задача 23

---
+++ Exercise_23/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] getFunction(int number)
+{
+    int [] array = new int[number];
+    int i = 1;
+    while(i <= number)
+    {
+        array[i - 1] = i * i * i;
+        Console.WriteLine(array[i - 1]);   
+        i++;
+    }    
+    return array;
+}
+int[] result = getFunction(5);
