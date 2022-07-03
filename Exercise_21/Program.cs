d1eabe0255ddf33a6ccb37ee5d51a54174fccf22
LidiaVolkova <lidok86@gmail.com>
2022-07-04 00:49:03 +0300

    Задача 21

---
+++ Exercise_21/Program.cs
@@ -0,0 +1,9 @@
+﻿double coordinate(double x1, double y1, double z1, double x2, double y2, double z2)
+{
+    double result = 0;
+    result = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2));
+    return result;
+}
+
+double segment = coordinate(7,-5,0,1,-1,9);
+Console.WriteLine(segment);
