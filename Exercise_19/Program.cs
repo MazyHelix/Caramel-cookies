7566275609fa339f5f495ee6dbf4458dfd5955da
LidiaVolkova <lidok86@gmail.com>
2022-07-04 01:43:01 +0300

    Задача 19

---
+++ Exercise_19/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите число");
+string? number = Console.ReadLine();
+
+void сheckingNumber(string number)
+{
+    if (number[0]==number[4] || number[1]==number[3])
+    {
+        Console.WriteLine("Число " + number + " - палиндром");
+    }
+    else Console.WriteLine("Число " + number +  " - не палиндром");
+}
+
+if (number.Length == 5)
+{
+    сheckingNumber(number);
+}
+else Console.WriteLine("Введите пятизначное число");
