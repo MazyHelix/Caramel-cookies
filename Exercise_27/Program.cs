e45f5da0f9e81511301d49760fba40568fe07ae8
LidiaVolkova <lidok86@gmail.com>
2022-07-09 17:42:18 +0300

    Задача 27

---
+++ Exercise_27/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 27
+//  Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+int countNumber(int value)
+{
+    if (value < 10)
+        return value;
+    int digit = value % 10;
+    int nextValue = value / 10;
+    return digit + countNumber(nextValue);
+}
+
+int myNumber = 9012;
+int sum = countNumber(myNumber);
+Console.WriteLine("Сумма цифр в числе " + myNumber + " равна " + sum);
