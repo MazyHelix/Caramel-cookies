3ab5b19100beaf93c44f389ef4c19313a055b09e
LidiaVolkova <lidok86@gmail.com>
2022-07-18 14:44:57 +0300

    Задача 47

---
+++ Exercise_47/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 47
+//Задайте двумерный массив размером M×N, заполненный случайными вещественными числами.
+
+Console.WriteLine("Введите количество столбцов");
+int N = Convert.ToInt32(Console.ReadLine());
+
+double[,] array = new double[M, N];
+Random rnd = new Random();
+
+for (int i = 0; i < M; i++)
+{
+    for (int j =0; j < N; j++)
+    {
+        array[i, j] = rnd.NextDouble();
+        Console.Write( array[i,j] + " ");
+    }
+    Console.WriteLine();
+}
