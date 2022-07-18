a1204be0b615a34cbd1fcb4ced7f456b524795d5
LidiaVolkova <lidok86@gmail.com>
2022-07-18 14:46:05 +0300

    Задача 52

---
+++ Exercise_52/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 52
+// Задайте двумерный массив из целых чисел. 
+// Найдите среднее арифметическое элементов в каждом столбце.
+
+Console.WriteLine("Введите количество строк");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int[,] array = new int[M, N];
+Random rnd = new Random();
+
+for (int i = 0; i < M; i++)
+{
+    for (int j = 0; j < N; j++)
+    {
+        array[i, j] = rnd.Next(-10, 10);
+        Console.Write( array[i,j] + " ");
+    }
+    Console.WriteLine();
+}
+
+for (int j = 0; j < N; j++)
+{
+    double sum = 0;
+    for (int i = 0; i < M; i++)
+    {
+        sum += array[i, j];
+    }
+    Console.WriteLine("Среднее арифметечкое по столбцам равно " + sum / M);
+}
